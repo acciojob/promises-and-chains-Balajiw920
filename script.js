7fb807f8bcac35ae5957f0a64cab40c6d8f75b48
Balajiw920 <wbalaji2021@gmail.com>
2023-03-23 21:46:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+const form = document.querySelector('form');
+const ageInput = document.getElementById('age');
+const nameInput = document.getElementById('name');
+const submitBtn = document.getElementById('btn');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const age = parseInt(ageInput.value);
+  const name = nameInput.value;
+
+  if (!age || !name) {
+    alert('Please fill in both age and name');
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age > 18) {
+        resolve({ age, name });
+      } else {
+        reject();
+      }
+    }, 4000);
+  });
+
+  promise
+    .then((obj) => obj.age)
+    .then((age) => ({ canVote: true, age }))
+    .then(({ canVote, age }) => {
+      if (canVote) {
+        alert(`Welcome, ${name}. You can vote.`);
+      } else {
+        alert(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    })
+    .catch(() => {
+      alert(`Oh sorry ${name}. You aren't old enough.`);
+    });
+});
